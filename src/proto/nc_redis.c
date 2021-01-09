47b000e0ff797bc42a6adbf2e62f8846c255ab4c
何欢 <momo@hehuandeMacBook-Air.local>
2021-01-09 17:10:20 +0800

    support unlink command

--- src/proto/nc_redis.c
+++ src/proto/nc_redis.c
@@ -268,6 +268,7 @@ redis_argx(struct msg *r)
     switch (r->type) {
     case MSG_REQ_REDIS_MGET:
     case MSG_REQ_REDIS_DEL:
+    case MSG_REQ_REDIS_UNLINK:
         return true;
 
     default:
@@ -877,6 +878,10 @@ redis_parse_req(struct msg *r)
                 if (str6icmp(m, 'z', 's', 'c', 'o', 'r', 'e')) {
                     r->type = MSG_REQ_REDIS_ZSCORE;
                     break;
+                }
+
+                if (str6icmp(m, 'u', 'n', 'l', 'i', 'n', 'k')) {
+                    r->type = MSG_REQ_REDIS_UNLINK;
                 }
 
                 break;
@@ -2386,7 +2391,7 @@ redis_pre_coalesce(struct msg *r)
     switch (r->type) {
     case MSG_RSP_REDIS_INTEGER:
         /* only redis 'del' fragmented request sends back integer reply */
-        ASSERT(pr->type == MSG_REQ_REDIS_DEL);
+        ASSERT(pr->type == MSG_REQ_REDIS_DEL || pr-type == MSG_REQ_REDIS_UNLINK);
 
         mbuf = STAILQ_FIRST(&r->mhdr);
         /*
@@ -2639,6 +2644,9 @@ redis_fragment_argx(struct msg *r, uint32_t ncontinuum, struct msg_tqh *frag_msg
         } else if (r->type == MSG_REQ_REDIS_DEL) {
             status = msg_prepend_format(sub_msg, "*%d\r\n$3\r\ndel\r\n",
                                         sub_msg->narg + 1);
+        } else if (r->type == MSG_REQ_REDIS_UNLINK) {
+            status = msg_prepend_format(sub_msg, "*%d\r\n$6\r\nunlink\r\n",
+                                        sub_msg->narg + 1);
         } else if (r->type == MSG_REQ_REDIS_MSET) {
             status = msg_prepend_format(sub_msg, "*%d\r\n$4\r\nmset\r\n",
                                         sub_msg->narg + 1);
@@ -2672,6 +2680,7 @@ redis_fragment(struct msg *r, uint32_t ncontinuum, struct msg_tqh *frag_msgq)
     switch (r->type) {
     case MSG_REQ_REDIS_MGET:
     case MSG_REQ_REDIS_DEL:
+    case MSG_REQ_REDIS_UNLINK:
         return redis_fragment_argx(r, ncontinuum, frag_msgq, 1);
 
     case MSG_REQ_REDIS_MSET:
@@ -2790,6 +2799,7 @@ redis_post_coalesce(struct msg *r)
         return redis_post_coalesce_mget(r);
 
     case MSG_REQ_REDIS_DEL:
+    case MSG_REQ_REDIS_UNLINK:
         return redis_post_coalesce_del(r);
 
     case MSG_REQ_REDIS_MSET:
